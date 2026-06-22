830600e016187ebf18ab4e2497bae4a78ee33e03
rameshmarathi <rameshmarathi7765@gmail.com>
2026-06-21 23:47:19 -0400

    Time: 3 ms (82.34%) | Memory: 20.5 MB (61.22%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,8 +1,5 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         if len(nums) == 1:
             return
 
@@ -10,14 +7,16 @@ class Solution:
         while i < len(nums):
             if nums[i] == 0:
                 break
-            i+=1
 
-        if i==len(nums):
-            return
+            i+=1
+        if i == len(nums):
+            return 
+
         j = i+1
 
         while j < len(nums):
-            if nums[j] !=0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i +=1
-            j+=1
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j],  nums[i]
+                i += 1
+            j += 1
+    
